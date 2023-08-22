e8bde2adc3129274f817ed9a5ea72a2e595ac2de
Dugege007 <351080175@qq.com>
2023-08-22 22:33:01 +0800

    Initial commit

---
+++ Assets/Script/Shot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject.Instantiate(bulletPrefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = GameObject.Instantiate(bulletPrefab, transform.position, transform.rotation);
+            Rigidbody rd = bullet.GetComponent<Rigidbody>();
+            //rd.AddForce(Vector3.forward * 5000);
+            rd.velocity = Vector3.forward * 100;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Siki_HitBricks_Teacher-Siki
+ 课程名称：宇宙最简单Unity2021零基础入门教程-打砖块
